7e93d4c33e8877d775e60df73fe5d3f5c91d81cf
felipehenrique159 <felipecod6@hotmail.com>
2025-02-17 19:48:07 -0300

    feat: arquivos iniciais

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="w-full h-full flex items-center justify-center">
+      <Text className="text-red-500">teste</Text>
+    </View>
+  );
+}
